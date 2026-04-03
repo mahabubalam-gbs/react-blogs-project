1813b2c6af22f8655c9996e364c793e185db6785
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-04-03 21:31:00 +0600

    Blogs fake data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,11 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <div className="main-content">
+        <div className="left-content">
+          <Blogs></Blogs>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const Blogs = () => {
+    useEffect(() => {
+        fetch("blogs.json")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Blogs;
