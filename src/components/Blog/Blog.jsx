bc051f35e1d22421a99c0dc9c9174452b4b920fe
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-04-04 23:06:54 +0600

    Bookmark icon added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Blog = ({ blog }) => {
-    const { cover, title, author, author_img, posted_date } = blog;
+    const { cover, title, author, author_img, posted_date, reading_time } = blog;
     return (
         <div className='py-10 w-3/4'>
             <div className="card bg-base-100 shadow-sm w-full">
@@ -11,17 +11,24 @@ const Blog = ({ blog }) => {
                         src={cover}
                         alt="image" />
                 </figure>
-                <div className="author pt-6 flex">
-                    <div className="avatar">
-                        <div className="w-16 rounded-full">
-                            <img src={author_img} />
+                <div className="author pt-6 flex justify-between">
+                    <div className='flex'>
+                        <div className="avatar">
+                            <div className="w-16 rounded-full">
+                                <img src={author_img} />
+                            </div>
+                        </div>
+                        <div className='author-details pl-5'>
+                            <h3 className='text-lg text-gray-500 font-semibold'>{author}</h3>
+                            <h5 className='text-gray-600'>{posted_date}</h5>
                         </div>
                     </div>
-                    <div className='author-details pl-5'>
-                        <h3 className='text-lg text-gray-800 font-semibold'>{author}</h3>
-                        <h5 className='text-gray-600'>{posted_date}</h5>
+                    <div className='pr-5'>
+                        <i class="fa-regular fa-bookmark"></i>
+                        <div>Reading time {reading_time} min</div>
                     </div>
                 </div>
+
                 <div className="card-body">
                     <h2 className="card-title text-2xl">{title}</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro assumenda ut ea a facere nisi! Facilis necessitatibus possimus reiciendis eveniet temporibus alias a eius dicta nobis! Laboriosam porro commodi voluptas.</p>
